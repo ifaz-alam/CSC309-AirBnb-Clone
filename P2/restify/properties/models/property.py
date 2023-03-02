853231ed91b7edb09f4a6eb766fb46103309f8b9
cole <cole.richardson@mail.utoronto.ca>
2023-03-02 15:51:28 -0500

    added models for property some todo still

--- P2/restify/properties/models/property.py
+++ P2/restify/properties/models/property.py
@@ -2,10 +2,22 @@ from django.db import models
 from accounts.models import Account
 from django.contrib.contenttypes.fields import GenericRelation
 from comments.models import Comment
+from user import models.user
+from reservation import Reservation
 
 
 class Property(models.Model):
     owner = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='properties')
-    
-    
+    #images TODO
+    description = models.TextField()
+    rating = models.IntegerField()
+    location = models.DateField()
+    available_times= models.DateTimeField()
+    prices = models.JSONField()
+    max_guests = models.IntegerField()
+    bathrooms = models.IntegerField()
+    #current_status = models. TODO
+    current_renter = models.user()
+    reservations = models.ForeignKey(Reservation)
+    banned = models.user()
     comments = GenericRelation(Comment, related_query_name='property')
