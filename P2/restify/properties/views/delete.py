51cfe08b5eac9590963aaee75f2a7f2993a2f273
cole <cole.richardson@mail.utoronto.ca>
2023-03-12 00:19:24 -0500

    get single property and all properties and delete property are working

--- P2/restify/properties/views/delete.py
+++ P2/restify/properties/views/delete.py
@@ -1,4 +1,30 @@
 from rest_framework.response import Response
+from ..models import Property
 
-def deleteProperty(response):
-    pass
+def deleteProperty(request):
+    """Delete the Property with the associated primary key
+    Must be the owner of the account to delete the property. 
+    Returns response status 200 on success, 403 if they are not the owner of the property
+    required field: primary key of property
+    
+    example delete request:
+    {
+        "pk": "1"
+    }
+    """
+    #TODO: ensure user deleting this is the owner
+    data = request.data
+    required_fields = {"pk"}
+    #TODO: check for required fields and missing fields here
+    try:
+        pk_int = int(data['pk'])
+    except:
+        return Response({'error': 'expected pk to be an int, got something else'}, status=400)
+
+    try:
+        property=Property.objects.get(pk=pk_int)
+    except:
+        return Response({'error': 'Poperty does not exist'}, status=404)
+    
+    property.delete()
+    return Response({'success, property deleted'}, status=200)
