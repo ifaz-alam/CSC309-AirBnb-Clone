1daa1c54ea6efbc3e84ae023876cdf047d7980df
Austin Blackman <austin.blackman@mail.utoronto.ca>
2023-03-13 20:32:37 -0400

    Commands for the demo

---
+++ P2/comments.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+./restify/manage.py shell
+from accounts.models import Account
+from properties.models import Property
+from comments.models import Comment
+u = Account.objects.get(pk=1)
+p = u.properties.all()[0]
+new_comment = Comment(author=u, comment="Testing!", rating=5,  object_id = p.pk, content_object = p)
+new_comment.save()
+new_comment2= Comment(author=u, comment="Testing!", rating=5,  object_id = p.pk, content_object = p)
+new_comment2.save()
+new_comment3 = Comment(author=u, comment="Testing!", rating=5,  object_id = p.pk, content_object = p)
+new_comment3.save()
+new_comment4 = Comment(author=u, comment="Testing!", rating=5,  object_id = p.pk, content_object = p)
+new_comment4.save()
+new_comment5 = Comment(author=u, comment="Testing!", rating=5,  object_id = p.pk, content_object = p)
+new_comment5.save()
