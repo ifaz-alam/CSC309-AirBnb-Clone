b6b2169b65e35774a4e81fcd9cf58a6f04281644
cole <cole.richardson@mail.utoronto.ca>
2023-03-14 01:31:06 -0400

    added optional check to create, added check to delete, got image working

--- P2/restify/properties/views/update.py
+++ P2/restify/properties/views/update.py
@@ -1,4 +1,7 @@
 from rest_framework.response import Response
 
 def updateProperty(request):
+    #use request.user to get the user. 
     pass
+    #if request.user != property.owner:
+        #Repsonse (error your are not the owner) 
