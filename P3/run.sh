9c7c8f6c0595a7d9598a58e563b81dc8f41b7d7a
Ifaz A <45114241+ifaz-alam@users.noreply.github.com>
2023-04-09 18:10:22 -0400

    run.sh to delete migrations for you

--- P3/run.sh
+++ P3/run.sh
@@ -2,6 +2,8 @@
 source venv/bin/activate
 gnome-terminal -- bash -c "cd frontend/restify && npm start"
 
+rm -rf ./backend/restify/migrations/*
+rmdir ./backend/restify/migrations
 ./backend/restify/manage.py makemigrations accounts comments images notifications properties reservations
 ./backend/restify/manage.py migrate
 ./backend/restify/manage.py runserver
