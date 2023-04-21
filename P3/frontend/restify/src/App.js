d8ac6cf81fde658a2934188b37d8f6c55dceefae
cole <cole.richardson@mail.utoronto.ca>
2023-04-21 02:40:22 -0400

    added gallery and progressed on create and edit pages

--- P3/frontend/restify/src/App.js
+++ P3/frontend/restify/src/App.js
@@ -11,32 +11,39 @@ import ProfilePage from "./pages/profile";
 import NavigationBar from "./components/NavigationBar";
 import Footer from "./components/Footer";
 import Debug from "./components/Debug";
+import Gallery from "./pages/gallery";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-      />
+	return (
+		<BrowserRouter>
+			<link
+				rel="stylesheet"
+				href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+			/>
 
-      <UserContext.Provider value={useUserConext()}>
-        <NavigationBar />
-        <Debug />
-        <Routes>
-          <Route path="/">
-            <Route index element={<Home />} />
-            <Route path="/accounts">
-              <Route path="signup" element={<SignupPage />} />
-              <Route path="login" element={<LoginPage />} />
-              <Route path="profile/:profileUser" element={<ProfilePage />} />
-            </Route>
-          </Route>
-        </Routes>
-        <Footer />
-      </UserContext.Provider>
-    </BrowserRouter>
-  );
+			<UserContext.Provider value={useUserConext()}>
+				<NavigationBar />
+				<Debug />
+				<Routes>
+					<Route path="/">
+						<Route index element={<Home />} />
+						<Route path="/accounts">
+							<Route path="signup" element={<SignupPage />} />
+							<Route path="login" element={<LoginPage />} />
+							<Route
+								path="profile/:profileUser"
+								element={<ProfilePage />}
+							/>
+						</Route>
+						<Route path="/properties">
+							<Route path="gallery/:propertypk" element={<Gallery/>}/>
+						</Route>
+					</Route>
+				</Routes>
+				<Footer />
+			</UserContext.Provider>
+		</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ P3/frontend/restify/src/pages/gallery/index.jsx
@@ -0,0 +1,223 @@
+import { useParams } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import ProfileCommentSection from "../../components/ProfileCommentSection";
+
+const Gallery=() => {
+    const { propertypk } = useParams();
+    const [property, setProperty] = useState({});
+    let APIURL = "http://localhost:8000";
+    useEffect(() => {
+        console.log(propertypk)
+        async function fetchProfile() {
+			let request = await fetch(`${APIURL}/properties/property/?pk=${propertypk}&all=false`, {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+				},
+			})
+            let response = await request.json();
+            console.log(response)
+            if (request.status !== 200){
+                console.log("error!")
+            }
+            else{
+                setProperty(response)
+                console.log(property)
+            }
+        };
+        fetchProfile();
+    }, [])
+
+    const getStars = (rating) => {
+		
+
+		if (rating === 0) {
+			return (
+				<>
+					<span className="fa fa-star"></span>{" "}
+					<span className="fa fa-star"></span>{" "}
+					<span className="fa fa-star"></span>{" "}
+					<span className="fa fa-star"></span>{" "}
+					<span className="fa fa-star"></span>
+				</>
+			);
+		} else if (rating === 1) {
+			return (
+				<>
+					<span className="fa fa-star checked"></span>{" "}
+					<span className="fa fa-star"></span>{" "}
+					<span className="fa fa-star"></span>{" "}
+					<span className="fa fa-star"></span>{" "}
+					<span className="fa fa-star"></span>
+				</>
+			);
+		} else if (rating === 2) {
+			return (
+				<>
+					<span className="fa fa-star checked"></span>{" "}
+					<span className="fa fa-star checked"></span>{" "}
+					<span className="fa fa-star"></span>{" "}
+					<span className="fa fa-star"></span>{" "}
+					<span className="fa fa-star"></span>
+				</>
+			);
+		} else if (rating === 3) {
+			return (
+				<>
+					<span className="fa fa-star checked"></span>{" "}
+					<span className="fa fa-star checked"></span>{" "}
+					<span className="fa fa-star checked"></span>{" "}
+					<span className="fa fa-star"></span>{" "}
+					<span className="fa fa-star"></span>
+				</>
+			);
+		} else if (rating === 4) {
+			return (
+				<>
+					<span className="fa fa-star checked"></span>{" "}
+					<span className="fa fa-star checked"></span>{" "}
+					<span className="fa fa-star checked"></span>{" "}
+					<span className="fa fa-star checked"></span>{" "}
+					<span className="fa fa-star"></span>
+				</>
+			);
+		} else if (rating === 5) {
+			return (
+				<>
+					<span className="fa fa-star checked"></span>{" "}
+					<span className="fa fa-star checked"></span>{" "}
+					<span className="fa fa-star checked"></span>{" "}
+					<span className="fa fa-star checked"></span>{" "}
+					<span className="fa fa-star checked"></span>
+				</>
+			);
+		}
+	};
+    const updateRating = (newRating) => {
+		console.log("updating rating now")
+        if (isNaN(newRating)) {
+			newRating = 0;
+		}
+        console.log(newRating)
+		setProperty({
+			...property,
+			rating: newRating,
+		});
+		console.log(property.rating);
+
+		fetch(`${APIURL}/properties/property/`, {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `${localStorage.getItem("Authorization")}`,
+			},
+			body: JSON.stringify({
+				...property,
+                owner: property.owner.pk,
+                images: property.images.pk,
+				rating: newRating
+			}),
+
+		})
+			.then((response) => {
+                console.log("something testing 2 or something")
+                return response.json()})
+
+			.then((result) => {
+				console.log("Success:", result);
+                console.log("expected", newRating)
+			});
+	};
+    return(<main>
+        {Object.keys(property).length > 0 ?(<div><div className="carouselwrapper">
+        {console.log(property)}
+        <div id="carouselExample" className="carousel slide">
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img src={`${APIURL}${property.images.image}`}/>
+              </div>
+              <div className="carousel-item">
+              <img src={`${APIURL}${property.images.image}`}/>
+              </div>
+              <div className="carousel-item">
+              <img src={`${APIURL}${property.images.image}`}/>
+              </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Next</span>
+            </button>
+          </div>
+    </div>
+    <div className="container-fluid">
+        <div className="row">
+            <h1 className="text-center mt-3 mb-3">
+                {property.name}
+            </h1>
+        </div>
+        <div className="row d-flex flex-row justify-content-center">
+            <div className="col d-flex justify-content-center">
+                <a href="./userpage.html">
+                    <h3>{property.owner.username}</h3>
+                </a>
+                <div className="mt-2 ms-3">
+                    {getStars(property.rating)}
+                </div>
+            </div>
+        </div>
+        <div className="row my-4">
+            <div className="col-lg-3">
+                <h4 className="text-center">Price Per Night</h4>
+                <ul className="list-group">
+                    <li
+                        className="list-group-item d-flex justify-content-between"
+                    >
+                        All Year
+                        <span className="fw-bold">{property.price_per_night}</span>
+                    </li>
+                </ul>
+            </div>
+            <div className="col-lg-6 mt-3 mt-lg-0">
+                <p className="ps-md-5 pe-md-5">
+                    {property.description}
+                </p>
+            </div>
+            <div
+                className="col-lg-3 mt-3 mt-lg-0 d-flex justify-content-center"
+            >
+                <div className="d-flex flex-column">
+                    <h5 className="text-center mb-4">
+                        Contact Information
+                    </h5>
+                    <h6 className="mb-2">
+                        {property.owner.email}
+                    </h6>
+                    <h6 className="text-center mb-2">{property.owner.phone_number}</h6>
+                    <button type="button" className="btn button-color">
+                        Book Now!
+                    </button>
+                    <a href="./edit-gallery.html" className="w-100">
+                        <button type="button" className="btn btn-danger mt-3 w-100">
+                            Edit Property
+                        </button>
+                    </a>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <div>
+        <ProfileCommentSection ParentType="Property"
+								ParentID={property.pk}
+								updateRating={updateRating}>
+        </ProfileCommentSection>
+        
+    </div>
+    </div>):(<div>Property not Found</div>)}
+    
+</main>)}
+export default Gallery;
