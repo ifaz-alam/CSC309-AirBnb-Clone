ad03a1df6e958387ca558896d0a1b8b90c88208c
Austin Blackman <austin.blackman@mail.utoronto.ca>
2023-04-21 18:36:42 -0400

    fixed

--- P3/frontend/restify/src/components/ProfilePropertyAcceptedToOthers/index.jsx
+++ P3/frontend/restify/src/components/ProfilePropertyAcceptedToOthers/index.jsx
@@ -25,39 +25,48 @@ const ProfilePropertyAcceptedToOthers = (props) => {
 		});
 		let response = await request.json();
 		console.log(response);
-		
+
 		// send the notification
 		setToggled(true);
-		sendNotification(owner, "cancellation_request", `http://localhost:8000/accounts/profile/${owner}`);
+		sendNotification(
+			owner,
+			"cancellation_request",
+			`http://localhost:8000/accounts/profile/${owner}`
+		);
 	};
 	if (toggled) {
 		<div className="card">
 			<div className="card-body d-flex justify-content-center align-items-center">
 				<h5 className="card-title">Cancelled</h5>
 			</div>
-		</div>
-
-	}
-	else {
+		</div>;
+	} else {
 		return (
 			<div className="card">
 				<img
 					src={`${APIURL}${picture}`}
 					alt={property.name}
 					className="card-img-top"
-					onClick={() => navigate(`/properties/gallery/${property.id}`)}
-					onMouseOver={(e) => (e.currentTarget.style.cursor = "pointer")}
+					onClick={() =>
+						navigate(`/properties/gallery/${property.id}`)
+					}
+					onMouseOver={(e) =>
+						(e.currentTarget.style.cursor = "pointer")
+					}
 				/>
 				<div className="card-body">
 					<h5 className="card-title">{property.name}</h5>
 					<p className="card-text">
-						Guest: <a href={`/accounts/profile/${owner}`}> {owner}</a>{" "}
+						Owner:{" "}
+						<a href={`/accounts/profile/${owner}`}> {owner}</a>{" "}
 					</p>
 					<p className="card-text">
 						Start Date: {reservation.start_date}
 					</p>
-					<p className="card-text">End Date: {reservation.end_date}</p>
-					<p className="card-text">State: pending</p>
+					<p className="card-text">
+						End Date: {reservation.end_date}
+					</p>
+					<p className="card-text">State: {reservation.state}</p>
 					<div className="buttons d-flex justify-content-evenly">
 						<button
 							className="btn btn-danger"
